238e68a59ed8cc414ae9abc69bc161ef5dd70dfe
NpNani <npnani25@gmail.com>
2023-01-01 21:52:14 -0500

    NEW ADDADED

--- pizza.py
+++ pizza.py
@@ -2,4 +2,10 @@ pizz = [1,2,3,4,5,6,7,8,9,10]
 
 for i in pizz:
     print(i)
-
+b = int(input("Enter the First Number: "))
+c = int(input("Enter the Second Number: "))
+if b > c:
+    print("B is Bigger than C: ")
+else:
+    print("Error")
+print("Simple code lol ")
